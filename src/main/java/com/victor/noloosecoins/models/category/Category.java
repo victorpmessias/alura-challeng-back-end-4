f677c03d7ce754a8814ac2c47b5853521fe2988c
Victor Messias <victor.p.messias@gmail.com>
2022-08-13 21:33:42 -0300

    Criado classe incial categoria

---
+++ src/main/java/com/victor/noloosecoins/models/category/Category.java
@@ -0,0 +1,13 @@
+package com.victor.noloosecoins.models.category;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
