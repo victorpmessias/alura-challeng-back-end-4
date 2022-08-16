4b416d71442601b25b5cc18cfaac7c288f056181
Victor Messias <victor.p.messias@gmail.com>
2022-08-16 13:14:12 -0300

    Adicionado busca de expenses por description e rota para buscar expenses por mes

--- src/main/java/com/victor/noloosecoins/repositories/ExpenseRepository.java
+++ src/main/java/com/victor/noloosecoins/repositories/ExpenseRepository.java
@@ -1,7 +1,14 @@
 package com.victor.noloosecoins.repositories;
 
 import com.victor.noloosecoins.models.expense.Expense;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    Page<Expense> findByDescriptionContains(String description, Pageable pageable);
+
+    Page<Expense> findByDateBetween(LocalDate startDate, LocalDate endDate, Pageable pageable);
 }
