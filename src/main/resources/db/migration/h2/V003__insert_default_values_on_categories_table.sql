94a54dab1b2a7f409b2211ffb7f1ba7c95bb576f
Victor Messias <victor.p.messias@gmail.com>
2022-08-22 16:18:41 -0300

    Fix

--- src/main/resources/db/migration/h2/V003__insert_default_values_on_categories_table.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO categories(name) values('Outras');
-INSERT INTO categories(name) values('Alimentação');
-INSERT INTO categories(name) values('Saúde');
-INSERT INTO categories(name) values('Moradia');
-INSERT INTO categories(name) values('Transporte');
-INSERT INTO categories(name) values('Lazer');
-INSERT INTO categories(name) values('Imprevistos');
