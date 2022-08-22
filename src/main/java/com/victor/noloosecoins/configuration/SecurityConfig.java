f9edeccf119815271385e502cfa1e99fe6d54456
Victor Messias <victor.p.messias@gmail.com>
2022-08-22 16:36:52 -0300

    Fix rota auth

--- src/main/java/com/victor/noloosecoins/configuration/SecurityConfig.java
+++ src/main/java/com/victor/noloosecoins/configuration/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.victor.noloosecoins.security.services.CredentialsService;
 import com.victor.noloosecoins.security.services.TokenService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/users").permitAll()
+                .antMatchers(HttpMethod.POST,"/auth").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
