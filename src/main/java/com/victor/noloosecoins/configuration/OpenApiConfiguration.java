57509caf293fffdcf7cc9840e04009b661f0b515
Victor Messias <victor.p.messias@gmail.com>
2022-08-24 16:11:38 -0300

    Adicionado configurações para fazer autentição via swagger

--- src/main/java/com/victor/noloosecoins/configuration/OpenApiConfiguration.java
+++ src/main/java/com/victor/noloosecoins/configuration/OpenApiConfiguration.java
@@ -5,7 +5,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @OpenAPIDefinition(info =
 @Info(
@@ -14,12 +19,23 @@ import org.springframework.context.annotation.Bean;
         description = "Documentation for API services and operations"
 )
 )
+@Configuration
 public class OpenApiConfiguration {
 
     @Bean
     OpenAPI customOpenApi() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .components(new Components())
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
                 .info(new io.swagger.v3.oas.models.info.Info()
                         .title("No-Loose-Coins Service API")
                         .version("V1")
